67bd258116fe48c1a8f983c6844c3c1384bffe62
Nebado <ruslan.nbd@gmail.com>
2021-06-14 23:57:05 +0300

    Add url params to handling

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,4 +1,18 @@
 import Dashboard from "./views/Dashboard.js";
+import Customers from "./views/Customers.js";
+import CustomerView from "./views/CustomerView.js";
+import Statistics from "./views/Statistics.js";
+
+const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+
+const getParams = match => {
+    const values = match.result.slice(1);
+    const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
+
+    return Object.fromEntries(keys.map((key, i) => {
+        return [key, values[i]];
+    }));
+};
 
 const navigateTo = (url) => {
   history.pushState(null, null, url);
@@ -6,30 +20,32 @@ const navigateTo = (url) => {
 };
 
 const router = async () => {
+    console.log(pathToRegex("posts/:id"));
     const routes = [
         { path: "/", view: Dashboard },
         { path: "/customers", view: Customers },
+        { path: "/customers/:id", view: CustomerView },
         { path: "/statistics", view: Statistics }
     ];
 
   // Test each route for potential match
   const potentialMatches = routes.map((route) => {
     return {
-      route: route,
-      isMatch: location.pathname === route.path,
+        route: route,
+        result: location.pathname.match(pathToRegex(route.path))
     };
   });
 
-  let match = potentialMatches.find((potentialMatch) => potentialMatch.isMatch);
+  let match = potentialMatches.find(potentialMatch => potentialMatch.result !== null);
 
   if (!match) {
     match = {
       route: routes[0],
-      isMatch: true,
+      result: [location.pathname],
     };
   }
 
-  const view = new match.route.view();
+    const view = new match.route.view(getParams(match));
 
   document.querySelector("#app").innerHTML = await view.getHtml();
 };
--- frontend/static/js/views/AbstractView.js
+++ frontend/static/js/views/AbstractView.js
@@ -1,11 +1,13 @@
 export default class {
-  consructor() {}
+    constructor(params) {
+        this.params = params;
+    }
 
-  setTitle(title) {
-    document.title = title;
-  }
+    setTitle(title) {
+        document.title = title;
+    }
 
-  async getHtml() {
-    return "";
-  }
+    async getHtml() {
+        return "";
+    }
 }
